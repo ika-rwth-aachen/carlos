a53e3f9cfa4211666df9108c9e5af2333745d1e7
Christian Geller <88664444+cgeller@users.noreply.github.com>
2024-02-05 21:07:39 +0100

    Add non-docker-compose demos to run-demo bash script (#7)

--- run-demo.sh
+++ run-demo.sh
@@ -9,10 +9,11 @@ if [ $# -eq 0 ]; then
   selected_demo=$default_demo
 else
   selected_demo=$1
-  if [ ! -f "$selected_demo/docker-compose.yml" ]; then
-    echo "No demo called $selected_demo exists. Please check available demos and run again."
-    exit 1
-  fi
+fi
+
+if [ "$selected_demo" != "data-driven-development" ] && [ "$selected_demo" != "automated-testing" ] && [ ! -f "$selected_demo/docker-compose.yml" ]; then
+  echo "No demo called $selected_demo exists. Please check available demos and run again."
+  exit 1
 fi
 
 # trap ctrl-c and killing the bash to call cleanup function
@@ -21,7 +22,9 @@ trap cleanup 0
 
 function cleanup() {
   echo "Cleaning up..."
-  $docker_compose_command -f $selected_demo/docker-compose.yml down
+  if [ "$selected_demo" != "data-driven-development" ] && [ "$selected_demo" != "automated-testing" ]; then
+    $docker_compose_command -f $selected_demo/docker-compose.yml down
+  fi
   xhost -local:
   echo "Done cleaning up."
 }
@@ -30,4 +33,20 @@ xhost +local:
 
 echo "Running demo: $selected_demo"
 
-$docker_compose_command -f $selected_demo/docker-compose.yml up 
+if [ "$selected_demo" = "data-driven-development" ]; then
+  env_name=$(grep 'name:' $selected_demo/env/environment.yml | awk '{print $2}')
+  conda_bin_dir=$(dirname $(which conda))
+  conda env list | grep "^${env_name} " > /dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    echo "Conda environment '$env_name' does not exist. Creating it..."
+    conda env create -f $selected_demo/env/environment.yml
+  fi
+  source $conda_bin_dir/activate $env_name
+  cd $selected_demo
+  python data_generation.py
+elif [ "$selected_demo" = "automated-testing" ]; then
+  cd $selected_demo
+  ./evaluate-scenarios.sh
+else
+  $docker_compose_command -f $selected_demo/docker-compose.yml up
+fi
