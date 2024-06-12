fea80466d204ea39b25449ad3bbc9fb1a37d57c1
Benedikt Haas <benedikt.haas@ika.rwth-aachen.de>
2024-06-12 15:49:46 +0200

    remove extracted action

--- .github/actions/evaluate-scenario/scripts/cleanup-carlos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-COMPOSE_FILE_PATH="${COMPOSE_FILE_PATH:-"$GITHUB_ACTION_PATH/compose.yml"}"
-
-# kill and remove remaining CARLOS setup
-docker compose -f $COMPOSE_FILE_PATH kill
-docker compose -f $COMPOSE_FILE_PATH down
